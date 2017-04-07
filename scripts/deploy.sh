59f158d03b1191e9a0fc5629d7f44d49b2e48161
Greg Rozmarynowycz <crashtest1@live.com>
2017-04-07 08:47:42 -0400

    add hidden files (e.x. htaccess) to deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 echo 'begin deploy'
 # if hidden files are added, add capture them with .??*
-cd .tmp && tar -czvf ../dist.tar.gz * && cd ..
+cd .tmp && tar -czvf ../dist.tar.gz * .??* && cd ..
 node scripts/deploy
